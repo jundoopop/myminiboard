8c332380779e543537828601011aa9255a80b23b
jundoopop <jundolpop@gmail.com>
2021-07-14 22:44:16 +0900

    Created Repository

---
+++ src/main/java/happygoat/springapp/miniboard/MiniBoardApplication.java
@@ -0,0 +1,13 @@
+package happygoat.springapp.miniboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/happygoat/springapp/miniboard/MiniBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package happygoat.springapp.miniboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
